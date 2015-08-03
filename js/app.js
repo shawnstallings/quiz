62a9486565405ca46a536ea019feadbde20fa33f
Shawn Stallings <oxegeneral@yahoo.com>
2015-08-03 17:05:59 -0600

    final project subimt with full js

--- js/app.js
+++ js/app.js
@@ -2,23 +2,31 @@ var questionNumber = '';
 
 // Functions
 
-//Get gender from intro screen to feed final result as boy or girl
-function getGender() {
-    var gender = $("input[name='gender']:checked").val();
-    console.log(gender);
-        if (gender === undefined) {
-            var gender = "boy";
-            console.log("undefined = " + gender);
-        }
-        else if (gender === "Female") {
-        	var gender = "girl";
-        	console.log("Female = " + gender);
-        }
-        else if (gender === "Male") {
-        	var gender = "boy";
-        	console.log("Male = " + gender);
-        }
-}		
+//Change cookie size based on correct answers
+function getCookie() {
+    
+    var numberRight = $('.rightAnswer').length;
+   
+    
+    if (numberRight === 0) {
+        $(".cookie,.answer-cookie,.result-cookie").css({width: "40px"});
+    }
+    else if (numberRight === 1) {
+        $(".cookie,.answer-cookie,.result-cookie").css({width: "80px"});
+    }
+    else if (numberRight === 2) {
+        $(".cookie,.answer-cookie,.result-cookie").css({width: "120px"});
+    }
+    else if (numberRight === 3) {
+         $(".cookie,.answer-cookie,.result-cookie").css({width: "160px"});
+    }
+    else if (numberRight === 4) {
+        $(".cookie,.answer-cookie,.result-cookie").css({width: "180px"});
+    }
+    else {
+        $(".cookie,.answer-cookie,.result-cookie").css({width: "200px"});
+    }
+}     	
 
 //Reloads the page when user clicks Try Again at the end
 function reloadPage() {
@@ -31,7 +39,7 @@ $(function() {
     //Begin button to start the quiz
     $(".options").submit(function(event) {
         event.preventDefault();
-        getGender();
+        
         $(".introPage").hide();
         $(".questionPage").css({display: "block"});
         $(".question1").css({display: "block"});
@@ -53,10 +61,14 @@ $(function() {
 
             if (answer === correct) {
         	    $(".correct").css({display: "block"});
+                $(".score").append( $( "<div class='rightAnswer'>" ) );
+                getCookie();
         	}
             else {
         	    $(".wrong").css({display: "block"});
+                getCookie();
         	}
+        
     });
 
     //Submit the answer to the 2nd question
@@ -73,67 +85,154 @@ $(function() {
 
             if (answer === correct) {
         	    $(".correct").css({display: "block"});
+                $(".score").append( $( "<div class='rightAnswer'>" ) );
         	}
             else {
         	    $(".wrong").css({display: "block"});
-        	}            
+        	}
+        getCookie();            
     });
 
     //Submit the answer to the 3rd question
     $(".answers3").submit(function(event) {
         event.preventDefault();
-        
+        var correct = "Aggressive";
+        var answer = $("input[name='answer3']:checked").val();
+        console.log(answer);
         $(".question3").css({display: "none"});
-        $(".question4").css({display: "block"});
+        $(".bottom").css({display: "none"});
+        $(".footer").css({display: "none"});
         $(".paw3r").removeClass('paw3r').addClass('paw3');
         $(".paw4").removeClass('paw4').addClass('paw4r');
+
+            if (answer === correct) {
+                $(".correct").css({display: "block"});
+                $(".score").append( $( "<div class='rightAnswer'>" ) );
+            }
+            else {
+                $(".wrong").css({display: "block"});
+            }
+        getCookie();           
     });
     //Submit the answer to the 4th question
     $(".answers4").submit(function(event) {
         event.preventDefault();
-        
+        var correct = "GoodTime";
+        var answer = $("input[name='answer4']:checked").val();
+        console.log(answer);
         $(".question4").css({display: "none"});
-        $(".question5").css({display: "block"});
+        $(".bottom").css({display: "none"});
+        $(".footer").css({display: "none"});
         $(".paw4r").removeClass('paw4r').addClass('paw4');
         $(".paw5").removeClass('paw5').addClass('paw5r');
+
+             if (answer === correct) {
+                $(".correct").css({display: "block"});
+                $(".score").append( $( "<div class='rightAnswer'>" ) );
+            }
+            else {
+                $(".wrong").css({display: "block"});
+            }
+        getCookie();   
     });
     //Submit the answer to the 5th question
     $(".answers5").submit(function(event) {
         event.preventDefault();
-        
+        var correct = "BackAway";
+        var answer = $("input[name='answer5']:checked").val();
+        console.log(answer);
         $(".question5").css({display: "none"});
-        $(".result").css({display: "block"});
+        $(".bottom").css({display: "none"});
+        $(".footer").css({display: "none"});
         $(".paw5r").removeClass('paw5r').addClass('paw5');
-        
+
+            if (answer === correct) {
+                $(".final-correct").css({display: "block"});
+                $(".score").append( $( "<div class='rightAnswer'>" ) );
+            }
+            else {
+                $(".final-wrong").css({display: "block"});
+            }   
+        getCookie();
     });
     
-    //When user clicks on an answer radio button it will change the class for later reference
+    //When user clicks on an answer radio button it will change the class for reference in the next function
     $(".answer").on('click', function() {
     	console.log(this);
     	$(".answer").removeClass('selectedAnswer');
     	$(this).addClass('selectedAnswer')
     });
 
-    //User clicks continue on correct or wrong status screen. This brings up the next question
+    //User clicks continue on correct or wrong status screen. This brings up the next question screen
     $(".continue").click(function() {
     	$(".correct").css({display: "none"});
     	$(".wrong").css({display: "none"});
         
         var questionNumber = $('.selectedAnswer')[0].parentElement.id;
-        
-    	$("." + questionNumber).css({display: "block"});
-    	$(".bottom").css({display: "block"});
-        $(".footer").css({display: "inline-block"});
-        $(".answer").removeClass('selectedAnswer')
+            
+            if (questionNumber === "result") {
+                $("." + questionNumber).css({display: "block"});
+                $(".answer").removeClass('selectedAnswer');
+            }
+            else {
+    	        $("." + questionNumber).css({display: "block"});
+    	        $(".bottom").css({display: "block"});
+                $(".footer").css({display: "inline-block"});
+                $(".answer").removeClass('selectedAnswer');
+            }    
 
     });	
 
+    //Move to final score screen to see the quiz results
+    $(".getScore").click(function() {
+        $(".final-correct").css({display: "none"});
+        $(".final-wrong").css({display: "none"});
+        $(".result").css({display: "block"});
+        $(".answer").removeClass('selectedAnswer');
 
+        var gender = $("input[name='gender']:checked").val();
+        var numberRight = $('.rightAnswer').length;
+        console.log(gender);
+            if (gender === undefined) {
+                var gender = "boy";
+                console.log("undefined = " + gender);
+            }
+            else if (gender === "Female") {
+                var gender = "girl";
+                console.log("Female = " + gender);
+            }
+            else if (gender === "Male") {
+                var gender = "boy";
+                console.log("Male = " + gender);
+            }
+            
+            $(".result-answer").text("Good " + gender + "! You got " + numberRight + " out of 5 questions right.");
+               
+            //Change result photo, cookie and text based on correct answers
+            if (numberRight === 0) {
+                $(".correct5-photo").removeClass('correct5-photo').addClass('zerocorrect-photo');
+                $(".phrase").replaceWith('<p class="phrase">You get a MICRO cookie!&nbsp&nbsp<span class="result-cookie"></span>&nbsp&nbspThank you for playing!</p>');
+            }
+            else if (numberRight === 1) {
+                $(".correct5-photo").removeClass('correct5-photo').addClass('correct1-photo');
+                $(".phrase").replaceWith('<p class="phrase">You get a TINY cookie!&nbsp&nbsp<span class="result-cookie"></span>&nbsp&nbspThank you for playing!</p>');
+            }
+            else if (numberRight === 2) {
+                $(".correct5-photo").removeClass('correct5-photo').addClass('correct2-photo');
+                $(".phrase").replaceWith('<p class="phrase">You get a SMALL cookie!&nbsp&nbsp<span class="result-cookie"></span>&nbsp&nbspThank you for playing!</p>');
+            }
+            else if (numberRight === 3) {
+                $(".correct5-photo").removeClass('correct5-photo').addClass('correct3-photo');
+                $(".phrase").replaceWith('<p class="phrase">You get a MEDIUM cookie!&nbsp&nbsp<span class="result-cookie"></span>&nbsp&nbspThank you for playing!</p>');
+            }
+            else if (numberRight === 4) {
+                $(".correct5-photo").removeClass('correct5-photo').addClass('correct4-photo');
+                $(".phrase").replaceWith('<p class="phrase">You get a LARGE cookie!&nbsp&nbsp<span class="result-cookie"></span>&nbsp&nbspThank you for playing!</p>');
+            }
+            else {
+                $(".phrase").replaceWith('<p class="phrase">You get a LARGE cookie!&nbsp&nbsp<span class="result-cookie"></span>&nbsp&nbspThank you for playing!</p>');
+            }          
 
+    }); 
 
-
-
-    	//var goodbyeMessage = "Good " + gender +"! You got " + (5 - $('.wrongAnswer').length) + " out of 5 questions right.";
-
-    	
 })
